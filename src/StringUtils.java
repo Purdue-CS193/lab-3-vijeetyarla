011c782f68a8c0e41373d747d62a54508ff5cf9f
vijeetyarla <vyarlaga@purdue.edu>
2018-09-26 12:28:23 -0400

    Fixed the java files to pass the tests

--- src/StringUtils.java
+++ src/StringUtils.java
@@ -8,23 +8,17 @@ public class StringUtils {
      * Ex: a = "Sean ", b = "Flannery", returns "sflanner"
      */
     public static String makePurdueUsername(String firstName, String lastName) {
-        /* Generate the long version of the name first:
-            Ex: firstName = "Sean", lastName = "Flannery"
-                    --> res = "SFlannery"
-         */
         String res = firstName.substring(0,1) + lastName;
         /* Shorten the string to 8 characters
             Ex: res = "SFlannery"
                     --> res = "SFlanner"
          */
-        res.substring(0, 8);
-        /* Convert the name to lower-case
-            Ex: res = "SFlanner"
-                    --> res = "sflanner"
-         */
-        res.toLowerCase();
+        if(res.length()>9) {
+            res = res.substring(0, 8);
+            res = res.toLowerCase();
+        }
         /* return the result of our calculation */
-        return res;
+        return res.toLowerCase();
     }
 
     /**
@@ -36,6 +30,6 @@ public class StringUtils {
      */
     public static String replaceStudentUsername(String text, String username) {
         /* Replace every instance of the username with proper message */
-        return text.replaceAll(username, "[DATA EXPUNDGED]");
+        return text.replaceAll(username, "[DATA EXPUNGED]");
     }
 }
--- src/UtilTester.java
+++ src/UtilTester.java
@@ -3,7 +3,8 @@
 // ALT + ENTER to "Add Junit4 to Classpath". This will allow
 // you to run the test cases!
 
-import org.junit.*;
+import org.junit.Test;
+
 import static org.junit.Assert.assertEquals;
 
 public class UtilTester {
